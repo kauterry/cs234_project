be2ddf54d0c00cb499b0cd6e1017a3406ab297cb
Kaushik Ram Sadagopan <kauterry@gmail.com>
2019-03-17 20:21:51 -0700

    Implemented LinUCB with shuffled data

--- baseline.py
+++ baseline.py
@@ -19,7 +19,7 @@ dosage = valid_data[:, 34].astype(np.float)
 
 def dosage_discretize(dosage):
 	"""
-	Converts continuous dosage values to discretized values (0.0, 0.5, 0.1)
+	Converts continuous dosage values to discretized values (0, 1, 2)
 
 	@param dosage (numpy array): 1 dimensional numpy array or list of continuous dosage values
 	@returns discrete_dosage (numpy array): 1 dimensional numpy array of discrete dosage values
@@ -28,18 +28,18 @@ def dosage_discretize(dosage):
 	discrete_dosage = []
 	for dose in dosage:
 		if dose < low:
-			discrete_dosage.append(0.0)
+			discrete_dosage.append(0)
 		elif dose <= high:
-			discrete_dosage.append(0.5)
+			discrete_dosage.append(1)
 		else:
-			discrete_dosage.append(1.0)
+			discrete_dosage.append(2)
 	discrete_dosage = np.array(discrete_dosage)
 	return discrete_dosage
 
 right_dosage = dosage_discretize(dosage)
 
 # Fixed-dose
-pf_fixed = np.count_nonzero(right_dosage == 0.5)/num_patients
+pf_fixed = np.count_nonzero(right_dosage == 1)/num_patients
 
 # Imputation of VKORC1 SNPs
 for patient in valid_data:
@@ -180,6 +180,7 @@ prediction_pd = np.square(5.6044 - 0.2614*age_decades + 0.0087*height_cm + 0.012
 pd_dosage = dosage_discretize(prediction_pd)
 pf_pharma = np.count_nonzero(pd_dosage == true_dosage)/n_patient
 
+print (pf_fixed, pf_clinical, pf_pharma)
 
 # Feature engineering
 
@@ -213,6 +214,12 @@ ethnicity = []
 age_decades = []
 height_cm = []
 weight_kg = []
+indication = []
+diabetes = []
+heart = []
+valve = []
+smoker = []
+aspirin = []
 cyp2 = []
 vk3673 = []
 vk5808 = []
@@ -223,7 +230,7 @@ vk7566 = []
 vk961 = []
 bias = []
 
-def one_hot(feature, str1, str2, query):
+def vkorc1(feature, str1, str2, query):
 	if type(query) == float:
 		feature.append([0.0, 0.0, 0.0, 1.0])
 	elif query == str1:
@@ -232,6 +239,14 @@ def one_hot(feature, str1, str2, query):
 		feature.append([0.0, 1.0, 0.0, 0.0])
 	else:
 		feature.append([0.0, 0.0, 1.0, 0.0])
+
+def three_hot(feature, index):
+	if type(patient[index]) == float:
+		feature.append([0.0, 0.0, 1.0])
+	elif patient[index] == '1':
+		feature.append([1.0, 0.0, 0.0])
+	else:
+		feature.append([0.0, 1.0, 0.0])
 
 for patient in valid_data:
 	if type(patient[1]) == float:
@@ -255,9 +270,9 @@ for patient in valid_data:
 	else:
 		ethnicity.append([0.0, 0.0, 1.0])
 	if type(patient[4]) == float:
-		age_decades.append(median_age - 1)
+		age_decades.append([(median_age - 1)/8])
 	else:
-		age_decades.append(int(patient[4][0]) - 1)
+		age_decades.append([(float(patient[4][0]) - 1)/8])
 	if type(patient[5]) == float:
 		height_cm.append(height_avg)
 	else:
@@ -266,68 +281,137 @@ for patient in valid_data:
 		weight_kg.append(weight_avg)
 	else:
 		weight_kg.append(float(patient[6]))
-	if patient[37] == '*1/*1':
-		cyp2.append([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-	elif patient[37] == '*1/*2':
-		cyp2.append([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-	elif patient[37] == '*1/*3':
-		cyp2.append([0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0])
-	elif patient[37] == '*2/*2':
-		cyp2.append([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])		
-	elif patient[37] == '*2/*3':
-		cyp2.append([0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0])
-	elif patient[37] == '*3/*3':
-		cyp2.append([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
+	if type(patient[7]) == float:
+		array = np.zeros(9)
+		array[0] = 1.0
+		indication.append(array.tolist())
 	else:
-		cyp2.append([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
-	one_hot(vk3673, 'A/A', 'A/G', patient[41])
-	one_hot(vk5808, 'G/G', 'G/T', patient[43])
-	one_hot(vk6484, 'C/C', 'C/T', patient[45])
-	one_hot(vk6853, 'C/C', 'C/G', patient[47])
-	one_hot(vk9041, 'A/A', 'A/G', patient[49])
-	one_hot(vk7566, 'C/C', 'C/T', patient[51])
-	one_hot(vk961, 'A/A', 'A/C', patient[53])
+		array = np.zeros(9)
+		lis = [int(c) for c in list(patient[7]) if c.isdigit()]
+		array[lis] = 1.0
+		indication.append(array.tolist())
+	three_hot(diabetes, 9)
+	three_hot(heart, 10)
+	three_hot(valve, 11)
+	three_hot(smoker, 36)
+	three_hot(aspirin, 13)
+	if patient[37] == '*1/*1':
+		array = np.zeros(12)
+		array[0] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*2':
+		array = np.zeros(12)
+		array[1] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*3':
+		array = np.zeros(12)
+		array[2] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*5':
+		array = np.zeros(12)
+		array[3] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*6':
+		array = np.zeros(12)
+		array[4] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*11':
+		array = np.zeros(12)
+		array[5] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*13':
+		array = np.zeros(12)
+		array[6] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*1/*14':
+		array = np.zeros(12)
+		array[7] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*2/*2':
+		array = np.zeros(12)
+		array[8] = 1
+		cyp2.append(array.tolist())		
+	elif patient[37] == '*2/*3':
+		array = np.zeros(12)
+		array[9] = 1
+		cyp2.append(array.tolist())
+	elif patient[37] == '*3/*3':
+		array = np.zeros(12)
+		array[10] = 1
+		cyp2.append(array.tolist())
+	else:
+		array = np.zeros(12)
+		array[11] = 1
+		cyp2.append(array.tolist())
+	vkorc1(vk3673, 'A/A', 'A/G', patient[41])
+	vkorc1(vk5808, 'G/G', 'G/T', patient[43])
+	vkorc1(vk6484, 'C/C', 'C/T', patient[45])
+	vkorc1(vk6853, 'C/C', 'C/G', patient[47])
+	vkorc1(vk9041, 'A/A', 'A/G', patient[49])
+	vkorc1(vk7566, 'C/C', 'C/T', patient[51])
+	vkorc1(vk961, 'A/A', 'A/C', patient[53])
 	bias.append([1.0])
 
-age_decades = np.array(age_decades)
-age_bins = 9
-age_features = np.zeros((num_patients, age_bins))
-age_features[np.arange(num_patients), age_decades] = 1.0
+h_max = max(height_cm)
+h_min = min(height_cm)
+height_cm = np.array(height_cm)
+height_features = (height_cm - h_min)/(h_max - h_min)
 
-height_bins = 8
-height_discrete = np.array([min(int((height - 120)/10), height_bins - 1) for height in height_cm])
-height_features = np.zeros((num_patients, height_bins))
-height_features[np.arange(num_patients), height_discrete] = 1.0
+w_max = max(weight_kg)
+w_min = min(weight_kg)
+weight_kg = np.array(weight_kg)
+weight_features = (weight_kg - w_min)/(w_max - w_min)
 
-weight_bins = 10
-weight_discrete = np.array([min(int((weight - 30)/20), weight_bins - 1) for weight in weight_kg])
-weight_features = np.zeros((num_patients, weight_bins))
-weight_features[np.arange(num_patients), weight_discrete] = 1.0
+features = np.array([g + r + e + age + ind + dia + hrt + val + sm + asp + c + v1 + v2 + v3 + v4 + v5 + v6 + v7 + b for g, r, e, age, ind, dia, hrt, val, sm, asp, c, v1, v2, v3, v4, v5, v6, v7, b in \
+zip(gender, race, ethnicity, age_decades, indication, diabetes, heart, valve, smoker, aspirin, cyp2, vk3673, vk5808, vk6484, vk6853, vk9041, vk7566, vk961, bias)])
 
-features = np.array([g + r + e + c + v1 + v2 + v3 + v4 + v5 + v6 + v7 + b for g, r, e, c, v1, v2, v3, v4, v5, v6, v7, b in \
-zip(gender, race, ethnicity, cyp2, vk3673, vk5808, vk6484, vk6853, vk9041, vk7566, vk961, bias)])
+features = np.column_stack((height_features, weight_features, features, right_dosage))
 
-features = np.column_stack((features, age_features, height_features, weight_features))
+# print (set(valid_data[:, 12].tolist()))
 
+#Shuffle patients
+runs = 10
+T = num_patients
 
+regret = np.zeros((T, runs))
+avg_incorrect = np.zeros((T, runs))
+pf_linucb = np.zeros(runs)
 
+for n in range(runs):
 
+	np.random.shuffle(features)
+	ground_truth = features[:, -1]
+	x_input = features[:, :-1]
 
+	# LinUCB
+	alpha = 0.1
+	K = 3
+	d = x_input.shape[1]
+	A = np.eye(d).repeat(K).reshape(d, d, K)
+	b = np.zeros((d, K))
+	theta = np.zeros((d, K))
+	p = np.zeros(K)
+	# linucb_pred = np.zeros(T)
 
+	#Training
+	sum_incorrect = 0
+	for j in range(T):
+		x = x_input[j]
+		for i in range(K):
+			inv = np.linalg.inv(A[:, :, i])
+			theta[:, i] = np.matmul(inv, b[:, i])
+			ucb = np.dot(x, np.matmul(inv, x))
+			p[i] = np.dot(theta[:, i], x) + alpha*np.sqrt(ucb)
+		act = np.random.choice(np.flatnonzero(p == np.amax(p)))
+		# linucb_pred[j] = act
+		reward = -1 if ground_truth[j] != act else 0
+		regret[j, n] = regret[j-1, n] - reward if j != 0 else -reward
+		sum_incorrect += -reward 
+		avg_incorrect[j, n] = sum_incorrect/(j+1.0)
+		A[:, :, act] += np.outer(x, x)
+		b[:, act] += reward*x
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	pf_linucb[n] = 1.0 - sum_incorrect/num_patients
+	print (pf_linucb[n])
 
 
